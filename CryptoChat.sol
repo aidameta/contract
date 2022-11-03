5b9695023d0a16c76dc89c044b69d2ac16efd3fa
aidameta <117352828+aidameta@users.noreply.github.com>
2022-11-03 18:01:16 +0800

    Delete CryptoChat.sol

--- CryptoChat.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.0;
-
-import "https://github.com/chengzhongnan/erc721/src/contracts/tokens/nf-token-metadata.sol";
-import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-
